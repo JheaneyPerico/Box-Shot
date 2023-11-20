c0774ba20c485d230d5f311a5a83e4d04c366eb1
JheaneyPerico <jheaneyperico@gmail.com>
2023-11-20 12:34:18 -0700

    Updated enemy positions

--- src/myCode.c
+++ src/myCode.c
@@ -2,11 +2,20 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <string.h>
+#define PLAYER "P1"
+#define ENEMY "Zb"
 
 int splash_screen() {
 
 	clear();
-
+	
+	if (has_colors()){
+		start_color();
+		init_pair(1, COLOR_RED, A_NORMAL);
+	}
+	
+	attron(A_BOLD);
 	mvprintw(10,10, " ________  ________     ___    ___");
 	mvprintw(11,10, "|\\   __  \\|\\   __  \\   |\\  \\  /  /|");
 	mvprintw(12,10, "\\ \\  \\|\\ /\\ \\  \\|\\  \\  \\ \\  \\/  / /");
@@ -16,16 +25,22 @@ int splash_screen() {
 	mvprintw(16,10, "    \\|_______|\\|_______/__/ /\\ __\\");
 	mvprintw(17,10, "                       |__|/ \\|__|");
 
+	attron(COLOR_PAIR(1));
+	mvprintw(10, 58, " ________  ___  ___  ________  _________");
+	mvprintw(11, 58, "|\\       \\|\\  \\|\\  \\|\\   __  \\|\\___   ___\\");
+	mvprintw(12, 58, "\\ \\  \\___|\\ \\  \\\\\\  \\ \\  \\|\\  \\|___ \\  \\_|");
+	mvprintw(13, 58, " \\ \\_____  \\ \\   __  \\ \\  \\\\\\  \\   \\ \\  \\");
+	mvprintw(14, 58, "  \\|____|\\  \\ \\  \\ \\  \\ \\  \\\\\\  \\   \\ \\  \\");
+	mvprintw(15, 58, "    ____\\_\\  \\ \\__\\ \\__\\ \\_______\\   \\ \\__\\");
+	mvprintw(16, 58, "   |\\_________\\|__|\\|__|\\|_______|    \\|__|");
+	mvprintw(17, 58, "   \\|_________|");
 
-	mvprintw(10, 60, " ________  ___  ___  ________  _________");
-	mvprintw(11, 60, "|\\       \\|\\  \\|\\  \\|\\   __  \\|\\___   ___\\");
-	mvprintw(12, 60, "\\ \\  \\___|\\ \\  \\\\\\  \\ \\  \\|\\  \\|___ \\  \\_|");
-	mvprintw(13, 60, " \\ \\_____  \\ \\   __  \\ \\  \\\\\\  \\   \\ \\  \\");
-	mvprintw(14, 60, "  \\|____|\\  \\ \\  \\ \\  \\ \\  \\\\\\  \\   \\ \\  \\");
-	mvprintw(15, 60, "    ____\\_\\  \\ \\__\\ \\__\\ \\_______\\   \\ \\__\\");
-	mvprintw(16, 60, "   |\\_________\\|__|\\|__|\\|_______|    \\|__|");
-	mvprintw(17, 60, "   \\|_________|");
+	attroff(COLOR_PAIR(1));
 
+	mvprintw(25, 43, "WELCOME TO BOX SHOT");
+	mvprintw(27, 36, "SELECT DIFFICULTY AND PRESS ENTER");
+	
+	attroff(A_BOLD);
 
 	WINDOW *inputWin = newwin(11, 14, 30, 45);
 	wborder(inputWin, '|', '|', '-', '-', '+', '+', '+', '+');
@@ -41,7 +56,7 @@ int splash_screen() {
 	mvwprintw(inputWin, yStr + 2, xStr, "LEVEL 1");
 	mvwprintw(inputWin, yStr + 4, xStr, "LEVEL 2");
 	mvwprintw(inputWin, yStr + 6, xStr, "LEVEL 3");
-
+	
 	// Selecting button
 	int x = 10, y = 2;
 	mvwprintw(inputWin, y, x, "<");
@@ -137,7 +152,7 @@ void Player(){
 	}
 
 	attron(COLOR_PAIR(1));
-	mvprintw(playerY, playerX, "P1");
+	mvprintw(playerY, playerX, PLAYER);
 	attroff(COLOR_PAIR(1));
 
 	keypad(stdscr, true);
@@ -195,11 +210,28 @@ void Enemies(int MAX_ENEMIES){
 	int max_y, max_x;
 	getmaxyx(stdscr, max_y, max_x);
 
-	srand(time(NULL));
 
+	int start_x = (max_x - (max_x / 2)) / 2 - 12;
+	int start_y = (max_y - (max_y / 2)) / 2 - 2;
+
+	//WINDOW *bord = newwin(25, 80,start_x, start_y);
+	//box(bord, 0,0);
+	
+	srand(time(NULL));
+	
 	for (int i = 0; i < MAX_ENEMIES; ++i){
-		enemyX[i] = rand() % max_x;
-		enemyY[i] = rand() % max_y;
+		enemyX[i] = rand() % (max_x);
+		enemyY[i] = rand() % (max_y);
+		
+		if (enemyY[i] > start_y && enemyY[i] < start_y + 25){
+			while (!(enemyX[i] < start_x) && !(enemyX[i] > (start_x + 80)))
+				enemyX[i] = rand() % (max_x);
+		}
+		
+		if (enemyX[i] > start_x && enemyX[i] < start_x + 80){
+			while (!(enemyY[i] < start_y) && !(enemyY[i] > (start_y + 25)))
+				enemyY[i] = rand() % (max_y);
+		}
 	}
 
 	if (has_colors()){
--- src/myCode.h
+++ src/myCode.h
@@ -1,2 +1,5 @@
 int splash_screen();
 void display_level(int lvl);
+void Player();
+void Enemies(int MAX_ENEMIES);
+void upgrade_box(int MAX_NUM);
