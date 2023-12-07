04be0ce5ab647d51d35e5b2a70265f51125de318
JheaneyPerico <jheaneyperico@gmail.com>
2023-12-06 19:33:17 -0700

    Updated comments and documentation

--- src/character.h
+++ src/character.h
@@ -1,13 +1,4 @@
 #include <ncurses.h>
-/*
-typedef struct {
-	int x;
-	int y;
-	char symbol[20];
-	int initial_x;
-	int initial_y;
-} Character;
-*/
 void printCharacter(WINDOW *win, char symbol, int *yLoc, int *xLoc, int type);
 void moveCharacter(WINDOW *win, int *yLoc, int *xLoc, char *dir, int key);
 void printEnemy(WINDOW *win, char symbol, int *ZyLoc, int *ZxLoc, int type, int *hit);
